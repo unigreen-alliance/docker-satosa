cb0318d89ab1cebf2c4470de3a06e98696ffbd62
Leif Johansson <leifj@sunet.se>
2016-04-22 14:07:36 +0200

    start script improvements

--- start.sh
+++ start.sh
@@ -1,9 +1,26 @@
 #!/usr/bin/env bash
 
+if [ -z "${DATA_DIR}" ]; then
+   DATA_DIR=/etc/satosa
+fi
+
+if [ ! -d "${DATA_DIR}" ]; then
+   mkdir -p "${DATA_DIR}"
+fi
+
+if [ -z "${PROXY_PORT}" ]; then
+   PROXY_PORT="8000"
+fi
+
+if [ -z "${METADATA_DIR}" ]; then
+   METADATA_DIR="${DATA_DIR}"
+fi
+
 cd ${DATA_DIR}
 
 # generate metadata for front- (IdP) and back-end (SP) and write it to mounted volume
-make_satosa_saml_metadata.py proxy_conf.yaml
+
+make_satosa_saml_metadata.py -o ${METADATA_DIR} proxy_conf.yaml
 
 # start the proxy
 if [[ -f https.key && -f https.crt ]]; then # if HTTPS cert is available, use it
