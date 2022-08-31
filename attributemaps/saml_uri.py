c1389e3c7e70164fb03a36c3b1a1da0a30e76393
Ivan Kanakarakis <ivan.kanak@gmail.com>
2022-08-31 12:37:18 +0300

    Add custom mfa_verified attribute mapping
    Signed-off-by: Ivan Kanakarakis <ivan.kanak@gmail.com>

--- attributemaps/saml_uri.py
+++ attributemaps/saml_uri.py
@@ -1,1 +1,8 @@
 from saml2.attributemaps.saml_uri import MAP
+
+
+mfa_verified_name = "mfa_verified"
+mfa_verified_friendly_name = "mfa_verified"
+
+MAP["fro"][mfa_verified_name] = mfa_verified_friendly_name
+MAP["to"][mfa_verified_friendly_name] = mfa_verified_name
